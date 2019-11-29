f9dcecc4d55557a3d043550e5abec3816a633e7d
Lana <dzidic.lana@nsoft.com>
2019-11-29 22:31:48 +0100

    Separate articles routes

---
+++ src/router/articleRoutes.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    path: '/articles',
+    name: 'articles',
+    component: () => import(/* webpackChunkName: "articles" */ '@/views/Articles'),
+    meta: {
+      authRequired: true,
+    },
+  },
+  {
+    path: '/article/:id',
+    name: 'article',
+    component: () => import(/* webpackChunkName: "article" */ '@/views/Article.vue'),
+  },
+];
